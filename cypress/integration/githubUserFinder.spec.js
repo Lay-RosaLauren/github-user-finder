2e12ae7784e53a15b9128217c3167a88848bd771
Lay-RosaLauren <laurenpsi@hotmail.com>
2022-02-14 11:16:30 -0300

    github user

--- cypress/integration/githubUserFinder.spec.js
+++ cypress/integration/githubUserFinder.spec.js
@@ -34,34 +34,7 @@ describe("GitHub User Finder ", () => {
     it("goes back to the homepage", () => {
       cy.get(".arrow_svg__feather").click();
       cy.contains("button", "Pesquisar").should("be.visible");
-      //cy.url().should('be.equal', `${Cypress.config('baseUrl')}/`)
-      cy.get("@userInputField").should("be.visible");
-    });
-  });
-
-  context("Error scenarios", () => {
-    it("erros when rate limit is exceeded and redirects to the homepage", () => {
-      cy.intercept("GET", "https://api.github.com/users/Lay-RosaLauren", {
-        statusCode: 403,
-      }).as("getUser");
-      cy.visit("/");
-      cy.get('input[type="text"]#user')
-        .as("userInputField")
-        .type("Lay-RosaLauren{enter}");
-      cy.wait("@getUser");
-
-      cy.contains("Erro")
-        .should("be.visible")
-        .parent()
-        .find("p")
-        .should("have.text", "Usuário não encontrado. Redirecionando...");
-      cy.get("header")
-        .next()
-        .find("main main:contains(Usuário não encontrado)")
-        .should("have.length", 3)
-        .and("be.visible");
-
-      //cy.url().should('be.equal', `${Cypress.config('baseUrl')}/`)
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}`)
       cy.get("@userInputField").should("be.visible");
     });
   });
